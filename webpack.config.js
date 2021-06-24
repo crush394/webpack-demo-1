77f9452a8086cd41984cbfe0c036fe9471c26981
crush <crush_mar@163.com>
2021-06-24 10:18:30 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,23 @@
-var path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
 module.exports = {
   mode: 'development',
   entry:'./src/index.js',
   output:{
       filename:'[name].[contenthash].js'
+  },
+  plugins: [new HtmlWebpackPlugin({
+      title:'My App',
+      template:'src/assets/index.html'
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      }
+    ]
   }
+
   }
