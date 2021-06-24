56887eaf0cd19400c6977f098b98db4aef4db2d4
crush <crush_mar@163.com>
2021-06-25 00:19:26 +0800

    准备上传

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -13,5 +13,35 @@ module.exports = {
       title:'My App',
       template:'src/assets/index.html'
   }),
-]
+],
+    module: {
+      
+        rules: [
+            {
+                test: /\.styl$/,
+                loader: ["style-loader", "css-loader", "stylus-loader"]
+              },
+            {
+                test: /\.less$/,
+                loader: ["style-loader", "css-loader", "less-loader"]
+              },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ["file-loader"]
+              },
+            {
+                test: /\.scss$/i,
+                use: [
+                  "style-loader",
+                  "css-loader",
+                  {
+                    loader: "sass-loader",
+                    options: {
+                      implementation: require("dart-sass")
+                    }
+                  }
+                ]
+              }
+        ]
+    }
   }
